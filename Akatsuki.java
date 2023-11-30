7c492ffb9757cd8807600f7db5d1b3948999615a
4NF0 <148747658+4NF0@users.noreply.github.com>
2023-11-30 16:23:42 +0700

    commit

---
+++ Akatsuki.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihan60;
+
+/**
+ *
+ * @author nm
+ */
+public class Akatsuki {
+    protected String pakaian;
+    protected String tujuan;
+
+    public Akatsuki(String pakaian, String tujuan) {
+        this.pakaian = pakaian;
+        this.tujuan = tujuan;
+    }
+
+    public String getpakaian() {
+        return pakaian;
+    }
+
+    public String gettujuan() {
+        return tujuan;
+    }
+}
